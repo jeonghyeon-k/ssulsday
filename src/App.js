3b3a6f54a8b2765e854f2521e7af3552ae7856b4
김정현 <rlawjd147@gmail.com>
2019-11-14 16:02:39 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-=======
 import React from "react";
 
 export default function App (props) {
   return (
-    <h1>do not use app before load page</h1>
+   <h1>do not use app before load page</h1>
   );
 };
->>>>>>> 5e9c3d7f1ac602547fb41343e1908f099cfd7cf6
--- src/components/CommentComponets/commentCreate.js
+++ src/components/CommentComponets/commentCreate.js
@@ -8,8 +8,13 @@ function commentCreate() {
   return (
     <div className={cx("create_container")}>
       <input
+<<<<<<< HEAD:src/components/CommentComponets/commentCreate.js
         className={cx("create_content")}
         autoFocus
+=======
+        className={cx("createcommnet__form")}
+        autoFocus={true}
+>>>>>>> b5bd9fe... no message:src/views/components/CommentCreate/commentCreate.js
         placeholder='댓글을 입력하세요'
       />
     </div>
