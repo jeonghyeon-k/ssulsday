8217eb8030ec20baaf280f2a68b35dd2f347f331
김정현 <>
2019-11-15 19:49:18 +0900

    feat. 로그인 화면

--- src/views/components/Page/LoginPage/LoginPage.js
+++ src/views/components/Page/LoginPage/LoginPage.js
@@ -1,50 +1,40 @@
 import React from "react";
 import styles from "./LoginPage.scss";
 import classNames from "classnames/bind";
+import Icon from "../../Icon/Icon";
 import PropTypes from "prop-types";
-
 
 const cx = classNames.bind(styles);
 
-const LoginPage = ({ autologin_done }) => {
+const LoginPage = ({ logged }) => {
   return (
     <div className={cx("loginpage")}>
-      <div className={cx("loginpage__logo")}>
-        
+      <div className={cx("header")}>
+        <div className={cx("header__logo")}></div>
       </div>
-      <div className={cx("loginpage__input")}>
-        <input
-          className={cx("loginpage__input__email")}
-          placeholder='이메일 주소'
-        />
-        <input
-          className={cx("loginpage__input__password")}
-          placeholder='비밀번호'
-        />
+      <div className={cx("input")}>
+        <input className={cx("input__email")} placeholder='이메일 주소' />
+        <input className={cx("input__password")} placeholder='비밀번호' />
       </div>
-      <button className={cx("loginpage__loginbutton")}>로그인</button>
+      <div className={cx("bottom")}>
+        <button className={cx("bottom__loginbutton")}>로그인</button>
 
-      <div className={cx("loginpage__Autologin")}>
-        <input
-          type='checkbox'
-          className={cx("loginpage__Autologin__checkbox")}
-          id='Checkbox'
-          checked={autologin_done}
-        />
-        <label for='Checkbox'></label>
-        <span className={cx("loginpage__Autologin_span")}>자동로그인</span>
-      </div>
+        <div className={cx("bottom__Autologin")}>
+          <Icon type="test"/>
+          <span className={cx("bottom__Autologin_span")}>자동로그인</span>
+        </div>
 
-      <a href='/' className={cx("loginpage__forgotpassword")}>
-        비밀번호 찾기
-      </a>
-      <button className={cx("loginpage__signupbutton")}>회원가입</button>
+        <a href='/' className={cx("bottom__forgotpassword")}>
+          비밀번호 찾기
+        </a>
+        <button className={cx("bottom__signupbutton")}>회원가입</button>
+      </div>
     </div>
   );
 };
 
 LoginPage.prototype = {
-  autologin_done: PropTypes.bool
+  logged: PropTypes.bool
 };
 
 export default LoginPage;
