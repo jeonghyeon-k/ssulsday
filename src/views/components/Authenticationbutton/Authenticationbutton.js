78f807b7c44810ce2d944b4d2b1cf2566324da44
김정현 <>
2019-11-15 20:39:17 +0900

    docs. 사용자 인증 버튼 컴포넌트 style. 버튼 색상 추가

--- src/views/components/Authenticationbutton/Authenticationbutton.js
+++ src/views/components/Authenticationbutton/Authenticationbutton.js
@@ -1,18 +1,25 @@
 import React from "react";
 import classNames from "classnames/bind";
 import styles from "./Authenticationbutton.scss";
+import PropTypes from "prop-types";
 
-const Authenticationbutton = () => {
-  const cx = classNames.bind(styles);
+const cx = classNames.bind(styles);
+
+
+
+const Authenticationbutton = ({done}) => {
   
   return (
-    <div>
-      <input
-        className={cx("Authenticationbutton")}
-        placeholder='인증코드 받기'
-      />
-    </div>
+      <div className={cx("Authenticationbutton",done&&"done")}  > 인증코드받기</div>
   );
+};
+
+Authenticationbutton.propTypes = {
+  done: PropTypes.bool
+};
+
+Authenticationbutton.defaultProps = {
+  done: false
 };
 
 export default Authenticationbutton;
