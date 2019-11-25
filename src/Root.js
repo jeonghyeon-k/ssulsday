4bc2ed8707aef37440fc262d91ceb99ced90877b
김정현 <rlawjd147@gmail.com>
2019-11-25 22:47:05 +0900

    style. 댓글 입력 폼 변경 된 디자인으로 수정

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./views/App";
+import Update from "./views/pages/UpdateUserpage/UpdateUser"
 
 const Root = () => {
   return (
     <BrowserRouter>
-      <App />
+      <Update/>
     </BrowserRouter>
   );
 };
