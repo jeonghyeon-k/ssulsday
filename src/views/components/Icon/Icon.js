29f1c148514b50005ce43323d2a0f20e38e7f646
gilsangwoo <gsw2205@gmail.com>
2019-11-27 03:10:01 +0900

    [#21]/feat icon component, spinner component
    - type 으로 icon을 나열했습니다 수정이 필요하시면 수정하시오
    - spinner 는 icon 기반으로 만들었습니다.

--- src/views/components/Icon/Icon.js
+++ src/views/components/Icon/Icon.js
@@ -4,12 +4,55 @@ import PropTypes from "prop-types";
 import style from "./Icon.scss";
 const cx = className.bind(style);
 export default function Icon(props) {
-  return <i className={cx("icon", props.type, props.size)} title={props.alt} />;
+  if(props.src) {
+    return (
+      <img
+        src={props.src}
+        alt={props.alt}
+        className={cx(props.size, props.style, props.className)} 
+      />
+    )
+  }
+  return <i className={cx("icon", props.type, props.size, props.className)} title={props.alt} />;
 }
 Icon.propTypes = {
-  type: PropTypes.string.isRequired,
-  size: PropTypes.oneOf(["sm", "md", "lg"]),
-  alt: PropTypes.string
+  type: PropTypes.oneOf([
+    "message",
+    "location",
+    "viewer",
+    "heart-dimmed",
+    "tab-home",
+    "tab-location",
+    "tab-write",
+    "tab-mypage",
+    "location-2",
+    "location-3",
+    "navi",
+    "heart",
+    "home-black",
+    "location-black",
+    "white-black",
+    "arrow",
+    "close",
+    "check-dimmed",
+    "check",
+    "check--color",
+    "search",
+    "arrow-small",
+    "arrow-small-right",
+    "location-icon-hash",
+    "location-icon",
+    "big-search",
+    "spin",
+    "share",
+    "more",
+    "check--color-dimmed"
+  ]),
+  size: PropTypes.oneOf(["xsm", "sm", "md", "lmd", "lg"]),
+  alt: PropTypes.string,
+  src: PropTypes.string,
+  style: PropTypes.string,
+  className: PropTypes.string,
 };
 Icon.defaultProp = {
   type: "test",
