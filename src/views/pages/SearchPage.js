188f1a558f0bc0b31e545e2620435745d3cbda12
Lavegaa <cksgh405@naver.com>
2019-11-28 16:26:13 +0900

    app-12마무리

--- src/views/pages/SearchPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import SearchTemplate from "../components/search/SearchTemplate";
-import Header from "../components/Header";
-
-const PostPage = () => {
-  return <SearchTemplate />;
-};
-
-export default PostPage;
