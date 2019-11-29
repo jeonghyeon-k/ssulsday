e4a588221958634713feb0ce9006c26affe6e9db
Lavegaa <cksgh405@naver.com>
2019-11-30 00:44:51 +0900

    post api작업중

--- src/domain/Post.js
+++ src/domain/Post.js
@@ -1,26 +1,26 @@
 export default class Post {
-    static create(postInfo) {
-        return new Post(postInfo);
-    }
-    constructor(postInfo){
-        const {
-            id = "",
-            title = "",
-            content = "",
-            userId = "",
-            categoryId = 0,
-            latitude = 0,
-            longitude = 0,
-            hashtags = "",
-        } = postInfo;
+  static create(postInfo) {
+    return new Post(postInfo);
+  }
+  constructor(postInfo) {
+    const {
+      user_id = "",
+      post_title = "",
+      post_content = "",
+      username = "",
+      category_id = 0,
+      latitude = 0,
+      longitude = 0,
+      hashtags = ""
+    } = postInfo;
 
-        this._id = id;
-        this._title = title;
-        this._content = content;
-        this._userId = userId;
-        this._categoryId = categoryId;
-        this._latitude = latitude;
-        this._longitude = longitude;
-        this._hashtags = hashtags;
-    }
+    this._id = user_id;
+    this._post_title = post_title;
+    this._post_content = post_content;
+    this._username = username;
+    this._category_id = category_id;
+    this._latitude = latitude;
+    this._longitude = longitude;
+    this._hashtags = hashtags;
+  }
 }
