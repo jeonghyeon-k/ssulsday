b365ccb0c769e9438cfa37560ee072a121341d68
김정현 <rlawjd147@gmail.com>
2019-11-30 01:29:45 +0900

    [master]/chore package.lock 삭제

--- src/context/serverContext.js
+++ src/context/serverContext.js
@@ -1,5 +1,10 @@
 import Axios from "axios"
+<<<<<<< HEAD
 import { SERVER_URL } from "../config/serverConfig"
+=======
+const SERVER_URL = process.env.REACT_APP_SERVER_URL || "";
+const REST_API_KEY = process.env.REACT_APP_REST_API_KEY || "";
+>>>>>>> af8fb6c... [master]/chore env 파일 생성
 
 export const api = ({url, type = "get", param}) => {
     return Axios({
