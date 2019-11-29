312c3795c0af3d691cb91adcf3434f54a3fb0b97
김정현 <rlawjd147@gmail.com>
2019-11-30 06:41:04 +0900

    feat. 마이페이지- 문의하기

--- src/views/pages/Mypage/question/question.js
+++ src/views/pages/Mypage/question/question.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./question.scss";
+import styles from "./question.module.scss";
 import classNames from "classnames/bind";
 import Navbar from "../../../components/Navbar/Navbar";
 import PropTypes from "prop-types";
@@ -11,7 +11,8 @@ const question = () => {
   <div className={cx("templete")}>
     <Navbar/>
     <input className={cx("question__title")} placeholder="제목"/>
-    <input className={cx("question__content")} placeholder="내용을 작성해주세요." />
+    <hr className={cx("question__hr")}/>
+    <textarea className={cx("question__content")} placeholder="내용을 작성해주세요." />
    <Navbar/>
   </div>
   );
