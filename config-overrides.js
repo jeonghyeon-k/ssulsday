5e44faee4ed241b41e006a3baee68f56c85303d5
gilsangwoo <gsw2205@gmail.com>
2019-11-30 04:28:52 +0900

    [master]/chore add contentBase

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,7 @@
+var path = require("path");
 module.exports = function override(config, env) {
     config.devServer={
+        contentBase: path.resolve(__dirname, './src'),
         host: '0.0.0.0',
         disableHostCheck: true
     }
