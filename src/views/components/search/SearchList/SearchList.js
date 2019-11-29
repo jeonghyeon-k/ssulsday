4de1216a8310165d9ff03efa9f9b0d0f33d5411d
Lavegaa <cksgh405@naver.com>
2019-11-30 05:20:28 +0900

    searchPage에 API담음 해시검색에 따른 list를 얻을수 있다. 디테일 list와 연동이 필요

--- src/views/components/search/SearchList/SearchList.js
+++ src/views/components/search/SearchList/SearchList.js
@@ -4,11 +4,19 @@ import classNames from "classnames/bind";
 import PropTypes from "prop-types";
 import Icon from "../../Icon/Icon";
 
+import { ApiCardByHashTag } from "../../../../repository/CardRepository";
+
 const cx = classNames.bind(styles);
 
 const Search = ({ ...props }) => {
+  const handleSearchList = () => {
+    console.log(props.val);
+    ApiCardByHashTag({ searchKeyword: props.val }).then(data => {
+      console.log(data);
+    });
+  };
   return (
-    <div className={cx("tag")}>
+    <div className={cx("tag")} onClick={handleSearchList}>
       <Icon type="location-icon" className="location__icon" />
       <span className={cx("tag__text")}>{props.val}</span>
     </div>
