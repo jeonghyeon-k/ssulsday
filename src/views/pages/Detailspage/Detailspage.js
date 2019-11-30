fc066634834320d0858588230574f13a083e11a4
김정현 <>
2019-11-30 16:02:15 +0900

    feat. post 방식으로 수정

--- src/views/pages/Detailspage/Detailspage.js
+++ src/views/pages/Detailspage/Detailspage.js
@@ -53,7 +53,7 @@ const Detailspage = ({ ...props }) => {
       <DetailHeader history={props.history} />
       <Ellipse />
       <div className={cx("pageblock")}>
-        <div className={cx("title")}>{card.card_title}</div>
+        <div className={cx("title")}>{card.post_title}</div>
         <div className={cx("head")}>
           <div className={cx("head__username")}>{card.username}</div>
           <div className={cx("head__date")}>{props.date}</div>
@@ -67,7 +67,7 @@ const Detailspage = ({ ...props }) => {
           <div className={cx("head__viewcount")}>{card.viewcount}</div>
         </div>
         <hr className={cx("hrbar")} />
-        <div className={cx("cardcontent")}>{card.card_content}</div>
+        <div className={cx("cardcontent")}>{card.post_content}</div>
         <div className={cx("hashtag")}>{card.hashtags}</div>
         <div className={cx("placeicon")}>
           <Icon type="location" />
