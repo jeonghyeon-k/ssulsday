7d3362aa91868d70485c281aa9d3dce2b5e47e5c
김정현 <rlawjd147@gmail.com>
2019-12-03 23:55:43 +0900

    no message

--- src/context/serverContext.js
+++ src/context/serverContext.js
@@ -4,8 +4,12 @@ import { SERVER_URL } from "../config/serverConfig"
 =======
 const SERVER_URL = process.env.REACT_APP_SERVER_URL || "";
 const REST_API_KEY = process.env.REACT_APP_REST_API_KEY || "";
+<<<<<<< HEAD
 >>>>>>> af8fb6c... [master]/chore env 파일 생성
 
+=======
+console.log(process.env.REACT_APP_SERVER_URL);
+>>>>>>> 5c01062... no message
 export const api = ({url, type = "get", param}) => {
     return Axios({
         method: type,
