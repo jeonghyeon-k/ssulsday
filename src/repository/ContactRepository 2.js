bc5f22336f6f878f71f7d5f2d2f1e722003810a9
jhkim <rlawjd147@gmail.com>
2020-12-16 17:28:19 +0900

    - File

--- src/repository/ContactRepository 2.js
+++
@@ -1,9 +0,0 @@
-import { api } from "../utils/api"
-
-export const ApiSendContact = async (param) => {
-    return await api({
-        url: `/cms/contact/send_action.do`,
-        type: "post",
-        param
-    });
-}
